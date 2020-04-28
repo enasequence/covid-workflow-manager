f91e4c2d3f15c8cc8d62d1cace35bc5bca0732cd
Alexey Sokolov <alexey@ebi.ac.uk>
2020-04-28 11:59:49 +0100

    Code refactoring

--- inputdata/download_raw_data.py
+++ inputdata/download_raw_data.py
@@ -2,6 +2,8 @@ import sys
 import os
 import subprocess
 import datetime
+import yaml
+import json
 
 from pymongo import MongoClient
 
@@ -18,13 +20,9 @@ def main():
     for file_name, file_urls in files_to_download.items():
         # Updating status of sample
         sample = DB.samples.find_one({'id': file_name})
-        sample['export_from_ena']['date'].append(
-            datetime.datetime.now().strftime("%d %B, %Y - %H:%M:%S"))
-        sample['export_from_ena']['status'].append('download started')
-        DB.samples.update_one({'id': file_name}, {'$set': sample})
+        write_sample_status(sample, 'download started')
 
         # Starting download for every url
-        sample = DB.samples.find_one({'id': file_name})
         data_download_errors = list()
         completed_process_mkdir = subprocess.run(
             f"mkdir /raw_data/{file_name}", shell=True, capture_output=True)
@@ -32,9 +30,11 @@ def main():
             data_download_errors.append(
                 completed_process_mkdir.stderr.decode('utf-8'))
 
-        # Download should be finished for all files in file_urls
+        # Download should be finished for all files in file_urls,
+        # ex. 2 for pair-end and 1 for single-end reads
         download_finished_for_urls = 0
         for file_url in file_urls:
+            # 10 attempts to do authentication with ftp server
             for _ in range(10):
                 output_file = f"/raw_data/{file_name}/" \
                               f"{os.path.basename(file_url)}"
@@ -47,17 +47,68 @@ def main():
                 else:
                     download_finished_for_urls += 1
                     break
-        sample['export_from_ena']['date'].append(
-            datetime.datetime.now().strftime("%d %B, %Y - %H:%M:%S"))
-        sample['export_from_ena']['errors'].extend(data_download_errors)
+        write_sample_errors(sample, data_download_errors)
         if len(data_download_errors) > 0 and \
                 download_finished_for_urls != len(file_urls):
-            download_status = 'failed'
+            write_sample_status(sample, 'download failed')
         else:
-            # TODO: submit pipeline job from here (submit file_name with it)
-            download_status = 'download finished'
-        sample['export_from_ena']['status'].append(download_status)
+            write_sample_status(sample, 'download finished')
+            write_sample_status(sample, 'starting to submit pipeline job')
+
+            # Converting yaml to json as required by k8s api server
+            with open('/wms/pipeline/run_pipeline-job/'
+                      'jovian-pipeline-run-job.yaml', 'r') as f:
+                job_to_submit = yaml.load(f.read(), Loader=yaml.FullLoader)
+                # Assigning unique id to job (id of run)
+                job_to_submit['metadata']['name'] = f'jovian-pipeline-run-' \
+                                                    f'job-{file_name}'
+                job_to_submit = json.dumps(job_to_submit)
+
+            # Get token, required by k8s api server to submit jobs
+            get_token_process = subprocess.run(
+                "cat /var/run/secrets/kubernetes.io/serviceaccount/token",
+                shell=True, capture_output=True)
+            k8s_token = get_token_process.stdout.decode('utf-8')
+
+            submit_job_process = subprocess.run(
+                f'curl -X POST -sSk -H "Authorization: Bearer {k8s_token}" '
+                f'-H "Content-Type: application/json" '
+                f'https://$KUBERNETES_SERVICE_HOST:'
+                f'$KUBERNETES_PORT_443_TCP_PORT/apis/batch/v1/namespaces/'
+                f'default/jobs -d \'{job_to_submit}\'',
+                shell=True, capture_output=True)
+            submit_job_process_results = json.loads(
+                submit_job_process.stdout.decode('utf-8'))
+            if submit_job_process_results['status'] == 'Failure':
+                write_sample_status(sample, 'submitting pipeline job failed')
+            else:
+                write_sample_status(sample, 'submitting pipeline job succeed')
         DB.samples.update_one({'id': file_name}, {'$set': sample})
+
+
+def write_sample_status(sample, status):
+    """
+    This function will write date and log message to sample dict
+    :param sample: sample to write log to
+    :param status: status message
+    :return:
+    """
+    sample['export_from_ena']['date'].append(
+        datetime.datetime.now().strftime("%d %B, %Y - %H:%M:%S"))
+    sample['export_from_ena']['status'].append(status)
+
+
+def write_sample_errors(sample, errors):
+    """
+    This function will write date and log message to sample dict
+    :param sample: sample to write log to
+    :param errors: errors list
+    :return:
+    """
+    if len(errors) > 0:
+        sample['export_from_ena']['date'].append(
+            datetime.datetime.now().strftime("%d %B, %Y - %H:%M:%S"))
+        sample['export_from_ena']['errors'].extend(errors)
 
 
 def parse_file():
