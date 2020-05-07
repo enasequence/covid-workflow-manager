b39800815794fdd0fd70b7ab4481dbd84b8b42c5
Alexey Sokolov <alexey@ebi.ac.uk>
2020-05-07 14:07:26 +0100

    Updating installation scripts

--- databases/cronjob/update_databases.sh
+++ databases/cronjob/update_databases.sh
@@ -4,7 +4,6 @@ source activate Jovian_helper
 set -ue
 # Set paths
 DB_PATH_NT="/mnt/database/NT_database"
-DB_PATH_NR="/mnt/database/NR_database"
 DB_PATH_TAX="/mnt/database/taxdb"
 DB_PATH_NTAX="/mnt/database/new_taxdump"
 DB_PATH_KRONA="/mnt/database/krona_taxonomy"
@@ -15,34 +14,33 @@ DB_PATH_VHOST="/mnt/database/Virus-Host_interaction_db"
 cd "${DB_PATH_TAX}" || exit 1
 perl "${CONDA_PREFIX}"/bin/update_blastdb.pl --decompress taxdb
 
-# UPDATING MGKIT
-cd "${DB_PATH_MGKIT}" || exit 1
-bash "${CONDA_PREFIX}"/bin/download-taxonomy.sh ./
+cd "${DB_PATH_MGKIT}" || exit
+bash "${CONDA_PREFIX}"/bin/download-taxonomy.sh
+curl -o nucl_gb.accession2taxid.gz -L https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz
+curl -o nucl_gb.accession2taxid.gz.md5 -L https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz.md5
+md5sum -c nucl_gb.accession2taxid.gz.md5
 
-# UPDATING KRONA
-cd "${DB_PATH_KRONA}" || exit 1
+cd "${DB_PATH_KRONA}" || exit
 bash "${CONDA_PREFIX}"/opt/krona/updateTaxonomy.sh ./
 bash "${CONDA_PREFIX}"/opt/krona/updateAccessions.sh ./
 
-# UPDATING VHOST INTERACTION DB
-cd "${DB_PATH_VHOST}" || exit 1
+cd "${DB_PATH_VHOST}" || exit
 curl -o virushostdb.tsv -L ftp://ftp.genome.jp/pub/db/virushostdb/virushostdb.tsv
 
-# UPDATING NEWTAXDUMP DB
-cd "${DB_PATH_NTAX}" || exit 1
+cd "${DB_PATH_NTAX}" || exit
 curl -o new_taxdump.tar.gz -L https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz
 curl -o new_taxdump.tar.gz.md5 -L https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz.md5
-tar -xzf new_taxdump.tar.gz
-for file in *.dmp;
-    do awk '{gsub("\t",""); if(substr(\$0,length(\$0),length(\$0))=="|") print substr(\$0,0,length(\$0)-1); else print \$0}' \${file} > \${file}.delim;
-    done
+if md5sum -c new_taxdump.tar.gz.md5
+then
+  # the md5 is correct:
+  tar -xzf new_taxdump.tar.gz
+  for file in *.dmp; do awk '{gsub("\t",""); if(substr($0,length($0),length($0))=="|") print substr($0,0,length($0)-1); else print $0}' < ${file} > ${file}.delim; done
+else
+  # the md5 does not match!
+  echo "The md5sum does not match new_taxdump.tar.gz! Please try downloading again."
+fi
 
-### UPDATING BLAST NT
-cd "${DB_PATH_NT}" || exit 1
+cd "${DB_PATH_NT}" || exit
 perl "${CONDA_PREFIX}"/bin/update_blastdb.pl --decompress nt
-
-### UPDATING BLAST NR
-cd "${DB_PATH_NR}" || exit 1
-perl "${CONDA_PREFIX}"/bin/update_blastdb.pl --decompress nr
 
 exit 0
--- databases/job/install_databases.sh
+++ databases/job/install_databases.sh
@@ -1,10 +1,9 @@
 set +ue
-conda env create -f /git/envs/Jovian_helper_environment.yaml
+conda env create -f /git/bin/envs/Jovian_helper_environment.yaml
 source activate Jovian_helper
 set -ue
 # Set paths
 DB_PATH_NT="/mnt/database/NT_database"
-DB_PATH_NR="/mnt/database/NR_database"
 DB_PATH_TAX="/mnt/database/taxdb"
 DB_PATH_NTAX="/mnt/database/new_taxdump"
 DB_PATH_KRONA="/mnt/database/krona_taxonomy"
@@ -13,7 +12,6 @@ DB_PATH_VHOST="/mnt/database/Virus-Host_interaction_db"
 
 # Make folders
 mkdir -p ${DB_PATH_NT}
-mkdir -p ${DB_PATH_NR}
 mkdir -p ${DB_PATH_TAX}
 mkdir -p ${DB_PATH_NTAX}
 mkdir -p ${DB_PATH_KRONA}
@@ -25,7 +23,10 @@ cd "${DB_PATH_TAX}" || exit
 perl "${CONDA_PREFIX}"/bin/update_blastdb.pl --decompress taxdb
 
 cd "${DB_PATH_MGKIT}" || exit
-bash "${CONDA_PREFIX}"/bin/download-taxonomy.sh ./
+bash "${CONDA_PREFIX}"/bin/download-taxonomy.sh
+curl -o nucl_gb.accession2taxid.gz -L https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz
+curl -o nucl_gb.accession2taxid.gz.md5 -L https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz.md5
+md5sum -c nucl_gb.accession2taxid.gz.md5
 
 cd "${DB_PATH_KRONA}" || exit
 bash "${CONDA_PREFIX}"/opt/krona/updateTaxonomy.sh ./
@@ -37,15 +38,17 @@ curl -o virushostdb.tsv -L ftp://ftp.genome.jp/pub/db/virushostdb/virushostdb.ts
 cd "${DB_PATH_NTAX}" || exit
 curl -o new_taxdump.tar.gz -L https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz
 curl -o new_taxdump.tar.gz.md5 -L https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz.md5
-tar -xzf new_taxdump.tar.gz
-for file in *.dmp;
-    do awk '{gsub("\t",""); if(substr($0,length($0),length($0))=="|") print substr($0,0,length($0)-1); else print $0}' < ${file} > ${file}.delim;
-    done
+if md5sum -c new_taxdump.tar.gz.md5
+then
+  # the md5 is correct:
+  tar -xzf new_taxdump.tar.gz
+  for file in *.dmp; do awk '{gsub("\t",""); if(substr($0,length($0),length($0))=="|") print substr($0,0,length($0)-1); else print $0}' < ${file} > ${file}.delim; done
+else
+  # the md5 does not match!
+  echo "The md5sum does not match new_taxdump.tar.gz! Please try downloading again."
+fi
 
 cd "${DB_PATH_NT}" || exit
 perl "${CONDA_PREFIX}"/bin/update_blastdb.pl --decompress nt
-
-cd "${DB_PATH_NR}" || exit
-perl "${CONDA_PREFIX}"/bin/update_blastdb.pl --decompress nr
 
 exit 0
