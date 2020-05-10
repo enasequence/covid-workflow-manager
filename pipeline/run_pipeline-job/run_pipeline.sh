8c165e45999f922f90f59e1f0e09dd8cd4ef6ef1
Alexey Sokolov <alexey@ebi.ac.uk>
2020-05-10 13:09:47 +0100

    Updating metagenomic version of pipeline

--- pipeline/run_pipeline-job/run_pipeline.sh
+++ pipeline/run_pipeline-job/run_pipeline.sh
@@ -1,5 +1,7 @@
 FILENAME="$1"
 
+source /git/bin/includes/functions
+
 python update_samples_status.py "$FILENAME" "pipeline started"
 
 set +ue
@@ -8,11 +10,9 @@ conda env create -f /git/envs/Jovian_master_environment.yaml
 source activate Jovian_master
 set -ue
 
-source /git/bin/functions.sh
-
-PROFILE="profile"
-UNIQUE_ID=$(/git/bin/generate_id.sh)
-SET_HOSTNAME=$(/git/bin/gethostname.sh)
+PROFILE="config"
+UNIQUE_ID=$(/git/bin/includes/generate_id.sh)
+HOSTNAME=$(hostname)
 
 
 
@@ -22,32 +22,22 @@ mkdir -p "/output/$FILENAME"
 
 cd "/output/$FILENAME" || exit
 
-/git/bin/generate_sample_sheet.py "${INPUT_DIR}" > "sample_sheet.yaml"
+/git/bin/scripts/generate_sample_sheet.py "${INPUT_DIR}" > "sample_sheet.yaml"
 cp -r /git/bin ./
-cp -r /git/envs ./
+cp -r /git/config ./
+cp -r /git/docs ./
 cp -r /git/files ./
-cp -r /git/profile ./
-cp -r /git/rules ./
-cp /git/Snakefile ./
 cp -r /git/.git ./
 
 # turn off bash strict mode because snakemake and conda can't work with it properly
 set +ue
-echo -e "Jovian_run:\n    identifier: ${UNIQUE_ID}" > "profile/variables.yaml"
-echo -e "Server_host:\n    hostname: http://${SET_HOSTNAME}" >> "profile/variables.yaml"
-eval $(parse_yaml "profile/variables.yaml" "config_")
-snakemake -s Snakefile --profile "${PROFILE}"
+echo -e "Jovian_run:\n    identifier: ${UNIQUE_ID}" > "config/variables.yaml"
+echo -e "Server_host:\n    hostname: http://${HOSTNAME}" >> "config/variables.yaml"
+eval $(parse_yaml "config/variables.yaml" "config_")
+snakemake -s bin/Snakefile --profile "${PROFILE}"
 set -ue
 
-if [ -d data/tables ]; then
-  bash bin/virus_typing.sh NoV
-  bash bin/virus_typing.sh EV
-  bash bin/virus_typing.sh RVA
-  bash bin/virus_typing.sh HAV
-  bash bin/virus_typing.sh HEV
-  bash bin/virus_typing.sh PV
-  bash bin/virus_typing.sh Flavi
-fi
+bash /git/bin/scripts/virus_typing.sh "all"
 
 conda deactivate
 if [ -e results/snakemake_report.html ]; then
