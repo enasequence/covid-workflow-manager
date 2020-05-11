11ac3c95fac073e7a88158d8ba166eb1c45716c9
Alexey Sokolov <alexey@ebi.ac.uk>
2020-05-11 19:52:15 +0100

    Updating install scripts

--- pipeline/installation-job/install_dependencies.sh
+++ pipeline/installation-job/install_dependencies.sh
@@ -1,33 +1,18 @@
-source .env
-
 set +ue
 conda config --set channel_priority false
 conda env create -f /git/bin/envs/Jovian_master_environment.yaml
+conda init bash
+source /root/.bashrc
 conda activate Jovian_master
-${CONDA_PREFIX}/bin/python -m ipykernel install --user --name Jovian_master --display-name "Jovian"
-${CONDA_PREFIX}/bin/jupyter nbextension enable collapsible_headings/main --sys-prefix
-${CONDA_PREFIX}/bin/jupyter nbextension enable highlight_selected_word/main --sys-prefix
-${CONDA_PREFIX}/bin/jupyter nbextension enable codefolding/main --sys-prefix
-${CONDA_PREFIX}/bin/jupyter nbextension enable execute_time/ExecuteTime --sys-prefix
-${CONDA_PREFIX}/bin/jupyter nbextension enable spellchecker/main --sys-prefix
-${CONDA_PREFIX}/bin/jupyter nbextension enable toggle_all_line_numbers/main --sys-prefix
-${CONDA_PREFIX}/bin/jupyter nbextension enable freeze/main --sys-prefix
-${CONDA_PREFIX}/bin/jupyter nbextension enable code_font_size/code_font_size --sys-prefix
-${CONDA_PREFIX}/bin/jupyter nbextension enable hide_input_all/main --sys-prefix
-${CONDA_PREFIX}/bin/jupyter nbextension enable toc2/main --sys-prefix
-${CONDA_PREFIX}/bin/jupyter nbextension enable varInspector/main --sys-prefix
-${CONDA_PREFIX}/bin/jupyter nbextension enable splitcell/splitcell --sys-prefix
-${CONDA_PREFIX}/bin/jupyter nbextension enable init_cell/main --sys-prefix
-${CONDA_PREFIX}/bin/jupyter nbextension enable tree-filter/index --sys-prefix
-${CONDA_PREFIX}/bin/jupyter nbextension enable codefolding/edit --sys-prefix
 set -ue
 
 PROFILE="config"
 UNIQUE_ID=$(/git/bin/includes/generate_id)
-SET_HOSTNAME=$(/git/bin/gethostname.sh)
+HOSTNAME=$(hostname)
 
 # Installing of Jovian specifif conda environments
 cd /git || exit
+cp /root/.ncbirc ./
 touch sample_sheet.yaml
 echo -e "Jovian_run:\n    identifier: ${UNIQUE_ID}" > profile/variables.yaml
 echo -e "Server_host:\n    hostname: http://${HOSTNAME}" >> profile/variables.yaml
