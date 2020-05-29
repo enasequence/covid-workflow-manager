25eb9d7c987d15fbea029d60c37cb540b7ce5374
Alexey Sokolov <alexey@ebi.ac.uk>
2020-05-29 12:05:29 +0100

    Adding new routes to back-end proxy

--- samples-logs-back-end/app/main.py
+++ samples-logs-back-end/app/main.py
@@ -4,16 +4,46 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def samples():
+@app.route("/jovian")
+def jovian_samples():
     # Getting access to MongoDB
     client = MongoClient('mongodb://samples-logs-db-svc')
     db = client.samples
 
     results = list()
-    for sample in db.samples.find({}, {'_id': 0}):
+    for sample in db.samples.find({'pipeline_name': 'Jovian'}, {'_id': 0}):
         results.append(sample)
     return {'results': results}
+
+
+@app.route("/jovian/<run_id>")
+def jovian_samples_details(run_id):
+    # Getting access to MongoDB
+    client = MongoClient('mongodb://samples-logs-db-svc')
+    db = client.samples
+
+    return {'results': db.samples.find_one({'id': run_id}, {'_id': 0})}
+
+
+@app.route("/ont")
+def jovian_samples():
+    # Getting access to MongoDB
+    client = MongoClient('mongodb://samples-logs-db-svc')
+    db = client.samples
+
+    results = list()
+    for sample in db.samples.find({'pipeline_name': 'ONT'}, {'_id': 0}):
+        results.append(sample)
+    return {'results': results}
+
+
+@app.route("/ont/<run_id>")
+def jovian_samples_details(run_id):
+    # Getting access to MongoDB
+    client = MongoClient('mongodb://samples-logs-db-svc')
+    db = client.samples
+
+    return {'results': db.samples.find_one({'id': run_id}, {'_id': 0})}
 
 
 if __name__ == "__main__":
