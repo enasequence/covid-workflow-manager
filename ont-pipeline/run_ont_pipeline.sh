a63f584b9224e219ce0bf0ec52fb93eb6cf26475
Alexey Sokolov <alexey@ebi.ac.uk>
2020-06-03 15:39:00 +0100

    Fixing ont submission k8s ms

--- ont-pipeline/run_ont_pipeline.sh
+++ ont-pipeline/run_ont_pipeline.sh
@@ -8,4 +8,4 @@ cd /data/"${FILENAME}_output" || exit
 cp /git/nanopore_nextflow.nf ./
 cp /git/reference.fasta ./
 
-nextflow -C /nextflow.config run nanopore_nextflow.nf --REFERENCE "$REFERENCE" --INPUT "$INPUT" --NAME "${FILENAME}_consensus" --outdir "$OUTDIR" -with-report "${FILENAME}.html"
+nextflow -C /nextflow.config run nanopore_nextflow.nf --REFERENCE "$REFERENCE" --INPUT "$INPUT" --NAME "${FILENAME}_consensus" --RUN "$FILENAME" --USER "$USER" --PASSWORD "$PASSWORD" -with-report "${FILENAME}.html" -resume
