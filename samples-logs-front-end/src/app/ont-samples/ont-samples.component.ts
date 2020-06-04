1b5221af8487ca10af53b6bb49746ddc0ede9666
Alexey Sokolov <alexey@ebi.ac.uk>
2020-06-04 12:29:43 +0100

    Fixing issues

--- samples-logs-front-end/src/app/ont-samples/ont-samples.component.ts
+++ samples-logs-front-end/src/app/ont-samples/ont-samples.component.ts
@@ -65,10 +65,10 @@ export class OntSamplesComponent implements OnInit {
 
   getSummary(data: any) {
     for (const item of data) {
-      console.log(item);
       const enaImportStatus = this.getExportStatus(item.import_from_ena.status, 'ena_import');
       const pipelineAnalysisStatus = this.getExportStatus(item.pipeline_analysis.status, 'pipeline_analysis');
       const enaExportStatus = this.getExportStatus(item.export_to_ena.status, 'ena_export');
+      console.log(enaExportStatus);
       this.summary.ena_import[enaImportStatus] += 1;
       this.summary.pipeline_analysis[pipelineAnalysisStatus] += 1;
       this.summary.ena_export[enaExportStatus] += 1;
