a0c5262d838b15b482ceebd4405fe58835f3b4c4
Alexey Sokolov <alexey@ebi.ac.uk>
2020-09-24 12:18:28 +0100

    Updating database installers

--- jovian-databases/cronjob/update_databases.sh
+++ jovian-databases/cronjob/update_databases.sh
@@ -6,21 +6,22 @@ conda activate Jovian_helper
 set -ue
 
 # Set paths
-DB_PATH_NT="/mnt/database/NT_database"
-DB_PATH_TAX="/mnt/database/taxdb"
-DB_PATH_NTAX="/mnt/database/new_taxdump"
-DB_PATH_KRONA="/mnt/database/krona_taxonomy"
-DB_PATH_MGKIT="/mnt/database/mgkit_taxonomy"
-DB_PATH_VHOST="/mnt/database/Virus-Host_interaction_db"
+DB_PATH_NT="/database/NT_database"
+DB_PATH_TAX="/database/taxdb"
+DB_PATH_NTAX="/database/new_taxdump"
+DB_PATH_KRONA="/database/krona_taxonomy"
+DB_PATH_MGKIT="/database/mgkit_taxonomy"
+DB_PATH_VHOST="/database/Virus-Host_interaction_db"
 
-# Updating BLAST taxdb
-cd "${DB_PATH_TAX}" || exit 1
+# Downloading databases
+cd "${DB_PATH_TAX}" || exit
 perl "${CONDA_PREFIX}"/bin/update_blastdb.pl --decompress taxdb
 
 cd "${DB_PATH_MGKIT}" || exit
 bash "${CONDA_PREFIX}"/bin/download-taxonomy.sh
-curl -o nucl_gb.accession2taxid.gz -L https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz
-curl -o nucl_gb.accession2taxid.gz.md5 -L https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz.md5
+rm taxdump.tar.gz
+wget -O nucl_gb.accession2taxid.gz ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz
+wget -O nucl_gb.accession2taxid.gz.md5 https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz.md5
 md5sum -c nucl_gb.accession2taxid.gz.md5
 
 cd "${DB_PATH_KRONA}" || exit
@@ -28,16 +29,16 @@ bash "${CONDA_PREFIX}"/opt/krona/updateTaxonomy.sh ./
 bash "${CONDA_PREFIX}"/opt/krona/updateAccessions.sh ./
 
 cd "${DB_PATH_VHOST}" || exit
-curl -o virushostdb.tsv -L ftp://ftp.genome.jp/pub/db/virushostdb/virushostdb.tsv
+wget -O virushostdb.tsv ftp://ftp.genome.jp/pub/db/virushostdb/virushostdb.tsv
 
 cd "${DB_PATH_NTAX}" || exit
-curl -o new_taxdump.tar.gz -L https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz
-curl -o new_taxdump.tar.gz.md5 -L https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz.md5
+wget -O new_taxdump.tar.gz ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz
+wget -O new_taxdump.tar.gz.md5 https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz.md5
 if md5sum -c new_taxdump.tar.gz.md5
 then
   # the md5 is correct:
   tar -xzf new_taxdump.tar.gz
-  for file in *.dmp; do awk '{gsub("\t",""); if(substr($0,length($0),length($0))=="|") print substr($0,0,length($0)-1); else print $0}' < ${file} > ${file}.delim; done
+  for file in *.dmp; do gawk '{gsub("\t",""); if(substr($0,length($0),length($0))=="|") print substr($0,0,length($0)-1); else print $0}' < ${file} > ${file}.delim; done
 else
   # the md5 does not match!
   echo "The md5sum does not match new_taxdump.tar.gz! Please try downloading again."
