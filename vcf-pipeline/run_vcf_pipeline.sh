0b74e3336ad247e86fc96a8c23e79543469495da
Alexey Sokolov <alexey@ebi.ac.uk>
2020-11-26 13:48:21 +0000

    Fixing configs

--- vcf-pipeline/run_vcf_pipeline.sh
+++ vcf-pipeline/run_vcf_pipeline.sh
@@ -8,7 +8,10 @@ cp /git/workflow.nf ./
 nextflow -C /nextflow.config run workflow.nf --READS "$READS" --RUN_ID "$RUN_ID" -resume
 
 if [ -f "/data/${RUN_ID}_output/results/${RUN_ID}.coverage" ] && [ -f "/data/${RUN_ID}_output/results/${RUN_ID}.cons.fa" ] && [ -f "/data/${RUN_ID}_output/results/${RUN_ID}.annot.n.filtered_freq.vcf" ]; then
-rm "/data/${RUN_ID}_1.fastq.gz";
-rm "/data/${RUN_ID}_2.fastq.gz";
 rm -rf ./work;
 fi
+
+cd /data || exit
+tar -zcvf "${RUN_ID}_input.tar.gz" "${RUN_ID}_input"
+mv "${RUN_ID}_input.tar.gz" /output
+rm -rf "${RUN_ID}_input"
