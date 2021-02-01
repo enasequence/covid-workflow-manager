aac9d751aa4501411b4faa3ec4ffced608e449cc
Ross Thorne <rmwthorne@googlemail.com>
2021-02-01 15:26:10 +0000

    Extract logging functions and refactor dockerfile

--- vcf-pipeline/worker.py
+++ vcf-pipeline/worker.py
@@ -1,47 +1,51 @@
 #!/usr/bin/env python
 
-import subprocess
-import os
-import sys
-
-import rediswq
+import os, sys, subprocess, datetime
 from pymongo import MongoClient
 
+import rediswq
 
-CLIENT = MongoClient('mongodb://samples-logs-db-svc')
+
+CLIENT = MongoClient("mongodb://samples-logs-db-svc")
 DB = CLIENT.samples
 HOST = "redis"
 q = rediswq.RedisWQ(name="job2", host=HOST)
 
+
 def main():
     print(f"Worker with sessionID: {q.sessionID()}")
     print(f"Initial queue state: empty = {str(q.empty())}")
-    while not q.empty(): # this is never false, never completes
+    while True:
         item = q.lease(lease_secs=10, block=True, timeout=2)
         if item is not None:
             process_item(item)
         else:
-            print("Waiting for work")
+            continue
     print("Queue empty, exiting")
     sys.exit(0)
+
 
 def process_item(item):
     run_id = item.decode("utf-8")
     print(f"Working on {run_id}")
     sample = get_sample(run_id, DB)
+    log_status(run_id, f"Starting download for {run_id}")
     try:
-        links = sample.get('links')
+        links = sample.get("links")
     except KeyError:
-        log_error('No links in sample entry')
+        log_error(run_id, "No links in sample entry")
         raise
 
     try:
         subprocess.run(f"mkdir -p /data/{run_id}_input", shell=True)
-        for link in links:
-            subprocess.run(f"wget -P /data/{run_id}_input ftp://{link}", shell=True, check=True)
+        for count, link in enumerate(links, start=1):
+            # set the output file to force overwrite in case this is not the first download attempt
+            wget_args = f"--directory-prefix /data/{run_id}_input --no-verbose --output-document {run_id}_{count}.fastq.gz"
+            subprocess.run(f"wget {wget_args} ftp://{link}", shell=True, check=True)
     except Exception:
-        log_error('Download failed')
+        log_error(run_id, "Download failed")
         raise
+
     # Start nextflow
     subprocess.run(f"mkdir -p /data/{run_id}_output", shell=True)
     os.chdir(f"/data/{run_id}_output")
@@ -49,14 +53,16 @@ def process_item(item):
     READS = READS.replace("(", "{")
     READS = READS.replace(")", "}")
     READS = READS.replace(" ", "")
-    nextflow_command = f"../nextflow -C ../nextflow.config run " \
-                        f"../workflow.nf --READS {READS} " \
-                        f"--RUN_ID {run_id} --resume"
+    nextflow_command = (
+        f"../nextflow -C ../nextflow.config run "
+        f"../workflow.nf --READS {READS} "
+        f"--RUN_ID {run_id} --resume"
+    )
     subprocess.run(nextflow_command, shell=True)
     # clean temprorary files
     if os.path.exists(
-            f"/data/{run_id}_output/results/" \
-            f"{run_id}.annot.n.filtered_freq.vcf"):
+        f"/data/{run_id}_output/results/" f"{run_id}.annot.n.filtered_freq.vcf"
+    ):
         subprocess.run(f"rm -rf ./work", shell=True)
         os.chdir("/data")
         subprocess.run(f"tar -zcvf {run_id}_output.tar.gz {run_id}_output", shell=True)
@@ -69,16 +75,35 @@ def process_item(item):
         subprocess.run("rm -rf {run_id}_input", shell=True)
     q.complete(item)
 
+
 def get_sample(run_id, db):
     print(f"Fetching {run_id} record from the database")
-    sample = db.samples.find_one({'id': run_id})
+    sample = db.samples.find_one({"id": run_id})
     if sample is None:
         raise TypeError("Sample not found in database, aborting")
     return sample
 
-def log_error(message):
-    print(f"Error: {message}")
-    pass
+
+def log_status(run_id, status):
+    timestamp = datetime.datetime.now().strftime("%d %B, %Y - %H:%M:%S")
+    DB.samples.update_one(
+        {"id": run_id},
+        {
+            "$push": {
+                "pipeline_analysis.date": timestamp,
+                "pipeline_analysis.status": status,
+            }
+        },
+    )
+
+
+def log_error(run_id, message):
+    timestamp = datetime.datetime.now().strftime("%d %B, %Y - %H:%M:%S")
+    DB.samples.update_one(
+        {"id": run_id},
+        {"$push": {"pipeline_analysis.errors": f"{timestamp} -- {message}"}},
+    )
+
 
 if __name__ == "__main__":
     main()
