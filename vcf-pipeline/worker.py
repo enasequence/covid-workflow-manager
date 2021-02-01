52d5b5a1168ded26c22550a53ea40a29e9ea902d
Ross Thorne <rmwthorne@googlemail.com>
2021-02-01 16:34:54 +0000

    Refactor steps

--- vcf-pipeline/worker.py
+++ vcf-pipeline/worker.py
@@ -28,7 +28,24 @@ def main():
 def process_item(item):
     run_id = item.decode("utf-8")
     print(f"Working on {run_id}")
+
     sample = get_sample(run_id, DB)
+    download_files(run_id, sample)
+    run_pipeline(run_id)
+    clean_up(run_id)
+
+    q.complete(item)
+
+
+def get_sample(run_id, db):
+    print(f"Fetching {run_id} record from the database")
+    sample = db.samples.find_one({"id": run_id})
+    if sample is None:
+        raise TypeError("Sample not found in database, aborting")
+    return sample
+
+
+def download_files(run_id, sample):
     log_status(run_id, f"Starting download for {run_id}")
     try:
         links = sample.get("links")
@@ -39,27 +56,42 @@ def process_item(item):
     try:
         subprocess.run(f"mkdir -p /data/{run_id}_input", shell=True)
         for count, link in enumerate(links, start=1):
-            # set the output file to force overwrite in case this is not the first download attempt
-            wget_args = f"--directory-prefix /data/{run_id}_input --no-verbose --output-document {run_id}_{count}.fastq.gz"
+            wget_args = (
+                f"--directory-prefix /data/{run_id}_input "
+                f"--no-verbose "
+                # force overwrite in case this is not the first download attempt
+                f"--output-document {run_id}_{count}.fastq.gz "
+            )
             subprocess.run(f"wget {wget_args} ftp://{link}", shell=True, check=True)
     except Exception:
         log_error(run_id, "Download failed")
         raise
 
-    # Start nextflow
-    subprocess.run(f"mkdir -p /data/{run_id}_output", shell=True)
-    os.chdir(f"/data/{run_id}_output")
-    READS = f"/data/{run_id}_input/{run_id}_{1, 2}.fastq.gz"
-    READS = READS.replace("(", "{")
-    READS = READS.replace(")", "}")
-    READS = READS.replace(" ", "")
+
+def run_pipeline(run_id):
+    subprocess.run(f"mkdir -p /data/{run_id}_output", shell=True, check=True)
+    os.chdir(f"/data/{run_id}_output", check=True)
+    reads = clean_read_path(f"/data/{run_id}_input/{run_id}_{1, 2}.fastq.gz")
     nextflow_command = (
         f"../nextflow -C ../nextflow.config run "
-        f"../workflow.nf --READS {READS} "
+        f"../workflow.nf --READS {reads} "
         f"--RUN_ID {run_id} --resume"
     )
-    subprocess.run(nextflow_command, shell=True)
-    # clean temprorary files
+    try:
+        subprocess.run(nextflow_command, shell=True, check=True)
+    except Exception:
+        log_error(run_id, "Pipeline failed")
+        raise
+
+
+def clean_read_path(path):
+    """
+    Replaces braces with curly braces, and removes whitespace
+    """
+    return path.replace("(", "{").replace(")", "}").replace(" ", "")
+
+
+def clean_up(run_id):
     if os.path.exists(
         f"/data/{run_id}_output/results/" f"{run_id}.annot.n.filtered_freq.vcf"
     ):
@@ -73,15 +105,6 @@ def process_item(item):
         os.chdir("/data")
         subprocess.run("rm -rf {run_id}_output", shell=True)
         subprocess.run("rm -rf {run_id}_input", shell=True)
-    q.complete(item)
-
-
-def get_sample(run_id, db):
-    print(f"Fetching {run_id} record from the database")
-    sample = db.samples.find_one({"id": run_id})
-    if sample is None:
-        raise TypeError("Sample not found in database, aborting")
-    return sample
 
 
 def log_status(run_id, status):
