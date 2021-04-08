db8562e6c4815fbd4fceee75cba762700ecd3e77
Ross Thorne <10939854+rmwthorne@users.noreply.github.com>
2021-04-08 15:42:19 +0100

    Log when download fails

--- pangolin/annotate_lineages.py
+++ pangolin/annotate_lineages.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import subprocess, pandas as pd
+import subprocess, datetime, pandas as pd
 from pymongo import MongoClient
 
 DB_URI = "mongodb://samples-logs-db-svc"
@@ -12,21 +12,31 @@ def main():
     records = parse_results()
     save_to_db(records)
 
+
 def download():
     subprocess.run("rm -rf /data/*", shell=True)
-    subprocess.run(f"wget "
+    download = subprocess.run(
+        f"wget "
         f"ftp://ftp.ebi.ac.uk/pub/databases/covid19dataportal/"
         f"viral_sequences/sequences/{SEQ_FILE}.gz"
-        f" -O /data/{SEQ_FILE}.gz"
-        , shell=True
+        f" -O /data/{SEQ_FILE}.gz",
+        shell=True,
+        capture_output=True,
     )
+    if download.returncode != 0:
+        log_message(
+            f"There was an error downloading viral sequences "
+            f"for annotation: {download.stderr.decode('utf-8')}"
+        )
     subprocess.run(f"gunzip /data/{SEQ_FILE}.gz", shell=True)
+
 
 def analyse():
     subprocess.run(
         f"pangolin -o /data/output --tempdir /data/temp --min-length 1000 /data/{SEQ_FILE}",
         shell=True,
     )
+
 
 def parse_results():
     df = pd.read_csv("/data/output/lineage_report.csv")[["taxon", "lineage"]]
@@ -37,11 +47,27 @@ def parse_results():
         .to_dict("records")
     )
 
+
 def save_to_db(records):
     client = MongoClient(DB_URI)
     db = client.samples
     db.pangolin.drop()
     db.pangolin.insert_many(records)
+
+
+def log_message(message):
+    db = MongoClient(DB_URI).samples
+    timestamp = datetime.datetime.now().strftime("%d %B, %Y - %H:%M:%S")
+    db.samples.update_one(
+        {"id": "pangolin"},
+        {
+            "$push": {
+                "import.date": timestamp,
+                "import.errors": message,
+            }
+        },
+        {"upsert": True}
+    )
 
 
 if __name__ == "__main__":
