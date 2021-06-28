4659b5e8180cb7eb7a7cf202ee50c7e981c61d4a
Ross Thorne <10939854+rmwthorne@users.noreply.github.com>
2021-06-28 13:59:09 +0100

    Add minimal annotate for testing

--- pangolin/annotate_lineages.py
+++ pangolin/annotate_lineages.py
@@ -6,26 +6,30 @@ from pymongo import MongoClient
 
 DB_URI = "mongodb://samples-logs-db-svc"
 SEQ_ENDPOINT = "https://www.ebi.ac.uk/ebisearch/ws/rest/embl-covid19"
-SOURCE = os.environ.get("SOURCE") # Should be 'ftp' or 'rest'
 debug = os.environ.get("DEBUG")
 
 
 def main():
-    if SOURCE == "rest":
-        annotate_new_accessions(remove_old=False)
-    elif SOURCE == "ftp":
-        annotate_all_accessions(remove_old=False)
+    if debug == "True":
+        minimal_annotate()
     else:
-        print(f"SOURCE environment variable is {SOURCE}. Should be one of 'ftp', 'rest'. Exiting")
+        annotate_new_accessions(remove_old=True)
+
+
+def minimal_annotate():
+    dest_dir = "/data/debug"
+    prep_workdir(dest_dir, remove_old=True)
+    download_multifasta(["MN908947", "LR991698"], f"{dest_dir}/sequences/seqs_1.fa.gz")
+    analyse_multiple(dest_dir)
+    result = glob.glob(f"{dest_dir}/output/*.csv")[0]
+    df = pd.read_csv(result)
+    print(df)
 
 
 def annotate_new_accessions(remove_old: bool):
     print("Annotating only new records found via REST API")
     dest_dir = "/data/new"
-    subprocess.run(f"mkdir -p {dest_dir}", shell=True)
-    if remove_old:
-        subprocess.run(f"rm -rf {dest_dir}/sequences/*", shell=True)
-        subprocess.run(f"rm -rf {dest_dir}/output/*", shell=True)
+    prep_workdir(dest_dir, remove_old=remove_old)
 
     accessions = fetch_new_accessions(SEQ_ENDPOINT)
     download_rest(accessions, f"{dest_dir}/sequences")
@@ -36,39 +40,14 @@ def annotate_new_accessions(remove_old: bool):
     save_to_db(df, replace=False)
 
 
-def annotate_all_accessions(remove_old: bool):
-    print("Annotating bulk sequence download from FTP")
-    dest_dir = "/data/bulk"
-    seq_file = "sequences_fasta_latest.fa" # Do not include .gz extension
-    subprocess.run(f"mkdir -p {dest_dir}", shell=True)
-    if remove_old:
-        subprocess.run(f"rm -rf {dest_dir}/sequences/*", shell=True)
-        subprocess.run(f"rm -rf {dest_dir}/output/*", shell=True)
-        download_ftp(f"{dest_dir}/sequences", seq_file)
-
-    analyse(dest_dir, seq_file)
-    df = parse_results(f"{dest_dir}/output/lineage_report.csv")
-    print(f"Total new records to add: {len(df)}")
-    save_to_db(df, replace=True)
-
-
-def download_ftp(dest_dir: str, filename: str):
-    download = subprocess.run(
-        f"wget ftp://ftp.ebi.ac.uk/pub/databases/covid19dataportal/"
-        f"viral_sequences/sequences/{filename}.gz"
-        f" -O {dest_dir}/{filename}.gz",
-        shell=True,
-        capture_output=True,
-    )
-    print(download.args)
-    if download.returncode != 0:
-        print(download.stderr.decode('utf-8'))
-        log_error(
-            f"There was an error downloading viral sequences "
-            f"for annotation: {download.stderr.decode('utf-8')}"
-        )
-    unzip = subprocess.run(f"gunzip {dest_dir}/{filename}.gz", shell=True)
-    print(unzip.args)
+def prep_workdir(path:str, remove_old: bool):
+    subprocess.run(f"mkdir -p {path}", shell=True)
+    subdirs = ['sequences', 'output', 'temp']
+    for subdir in subdirs:
+        subprocess.run(f"mkdir -p {path}/{subdir}", shell=True)
+    if remove_old is True:
+        for subdir in subdirs:
+            subprocess.run(f"rm -rf {path}/{subdir}/*", shell=True)
 
 
 def fetch_new_accessions(endpoint: str) -> list:
@@ -86,8 +65,8 @@ def fetch_new_accessions(endpoint: str) -> list:
     # fetch all available sequence accessions
     entries = get_paginated_results(endpoint, limit=rest_accession_count)
     all_accessions = [x.get("acc") for x in entries]
-    new_accessions = set(all_accessions).difference(annotated_accesions)
     # Return accessions not already in database
+    new_accessions = set(all_accessions).difference(annotated_accesions)
     return list(new_accessions)
 
 
@@ -107,11 +86,11 @@ def download_rest(accessions: list, directory: str, chunk_size: int = 400):
 
     print(f"Batch downloading {len(accessions)} new accessions...")
     chunks = [accessions[x:x + chunk_size] for x in range(0, len(accessions), chunk_size)]
-    for i, chunk in enumerate(chunks):
-        download_from_rest(chunk, f"{directory}/seqs_{i}.fa.gz")
+    for i, accessions in enumerate(chunks):
+        download_multifasta(accessions, f"{directory}/seqs_{i}.fa.gz")
 
 
-def download_from_rest(accessions: list, filename: str) -> str:
+def download_multifasta(accessions: list, filename: str) -> str:
     start = datetime.datetime.now()
     urlretrieve(
         "https://www.ebi.ac.uk/ena/browser/api/fasta/textsearch?" + urlencode({
@@ -127,7 +106,7 @@ def download_from_rest(accessions: list, filename: str) -> str:
     return filename
 
 
-def get_paginated_results(url: str, limit: int, batch_size: int =1000) -> list:
+def get_paginated_results(url: str, limit: int, batch_size: int = 1000) -> list:
     parameter_list = [
         {
             "query": "id:[* TO *]",
@@ -141,25 +120,10 @@ def get_paginated_results(url: str, limit: int, batch_size: int =1000) -> list:
     return flatten(entry_list)
 
 
-def analyse(directory: str, seq_file: str):
-    process = subprocess.run(
-        f"pangolin "
-        f"--outdir {directory}/output "
-        f"--tempdir {directory}/temp "
-        f"--min-length 1000 "
-        f"{directory}/sequences/{seq_file}",
-        shell=True,
-    )
-    print(process.args)
-    with open(f"{directory}/pangolin.log", 'rw') as f:
-        f.write(process.stderr.decode('utf-8'))
-    report_errors(process)
-
-
 def analyse_multiple(directory: str):
     for g in glob.glob(f"{directory}/sequences/seqs_*.gz"):
-        gunzip = subprocess.run(f"gunzip {g}", shell=True)
-        report_errors(gunzip)
+        gunzip = subprocess.run(f"gunzip -f {g}", shell=True)
+        gunzip.check_returncode()
 
     for seq_file in glob.glob(f"{directory}/sequences/seqs_*.fa"):
         process = subprocess.run(
@@ -171,7 +135,8 @@ def analyse_multiple(directory: str):
             f"{seq_file}",
             shell=True
         )
-        report_errors(process)
+        process.check_returncode()
+
 
 def parse_results(csv_file: str) -> pd.DataFrame:
     print(f"parsing {csv_file}")
@@ -191,9 +156,6 @@ def save_to_db(df: pd.DataFrame, replace: bool):
         db.pangolin.drop()
     db.pangolin.insert_many(records)
 
-def report_errors(process: subprocess.CompletedProcess):
-    if process.returncode != 0:
-        print(process.stderr.decode("utf-8"))
 
 def log_error(message: str):
     db = MongoClient(DB_URI).samples
