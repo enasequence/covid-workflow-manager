185899b236f925dadb45ea5f224713202c4e7b00
Alexey Sokolov <alexey@ebi.ac.uk>
2021-07-22 11:38:40 +0100

    Adding skip and limit to kooplex proxy methods

--- kooplex-proxy/proxy/crud.py
+++ kooplex-proxy/proxy/crud.py
@@ -2,45 +2,45 @@ from sqlalchemy.orm import Session
 import models
 
 
-def get_covid_country_weekly(db: Session):
-    return db.query(models.CovidCountryWeekly).all()
+def get_covid_country_weekly(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.CovidCountryWeekly).offset(skip).limit(limit).all()
 
 
-def get_unique_vcf_append(db: Session):
-    return db.query(models.UniqueVCFAppend).all()
+def get_unique_vcf_append(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.UniqueVCFAppend).offset(skip).limit(limit).all()
 
 
-def get_vcf_all(db: Session):
-    return db.query(models.VCFAll).all()
+def get_vcf_all(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.VCFAll).offset(skip).limit(limit).all()
 
 
-def get_cov(db: Session):
-    return db.query(models.Cov).all()
+def get_cov(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Cov).offset(skip).limit(limit).all()
 
 
-def get_meta(db: Session):
-    return db.query(models.Meta).all()
+def get_meta(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Meta).offset(skip).limit(limit).all()
 
 
-def get_unique_cov_append(db: Session):
-    return db.query(models.UniqueCovAppend).all()
+def get_unique_cov_append(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.UniqueCovAppend).offset(skip).limit(limit).all()
 
 
-def get_lineage_def(db: Session):
-    return db.query(models.LineageDef).all()
+def get_lineage_def(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.LineageDef).offset(skip).limit(limit).all()
 
 
-def get_operation(db: Session):
-    return db.query(models.Operation).all()
+def get_operation(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Operation).offset(skip).limit(limit).all()
 
 
-def get_unique_cov(db: Session):
-    return db.query(models.UniqueCov).all()
+def get_unique_cov(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.UniqueCov).offset(skip).limit(limit).all()
 
 
-def get_unique_vcf(db: Session):
-    return db.query(models.UniqueVCF).all()
+def get_unique_vcf(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.UniqueVCF).offset(skip).limit(limit).all()
 
 
-def get_vcf_all_append(db: Session):
-    return db.query(models.VCFAllAppend).all()
+def get_vcf_all_append(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.VCFAllAppend).offset(skip).limit(limit).all()
