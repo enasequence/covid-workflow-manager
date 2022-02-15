a2711e0ae335659dacce7fd179cc271dd06bcb34
Milena Mansurova <mansurova@ebi.ac.uk>
2022-02-15 16:20:26 +0000

    fixes of deployment and credentials

--- kooplex-proxy/proxy/database.py
+++ kooplex-proxy/proxy/database.py
@@ -3,14 +3,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
 POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 POSTGRES_HOST = os.getenv('POSTGRES_HOST')
 POSTGRES_DB = os.getenv('POSTGRES_DB')
+POSTGRES_PORT = os.getenv('POSTGRES_PORT')
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://" \
                           f"{POSTGRES_USER}:{POSTGRES_PASSWORD}@" \
-                          f"{POSTGRES_HOST}/{POSTGRES_DB}"
+                          f"{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
