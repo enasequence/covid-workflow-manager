1fe7dd08d35e750067016cadc47e678841062cf3
Milena Mansurova <mansurova@ebi.ac.uk>
2023-01-06 05:25:45 +0300

    POSTGRES_SCHEMA environment variable adding

--- kooplex-proxy/proxy/database.py
+++ kooplex-proxy/proxy/database.py
@@ -9,12 +9,13 @@ POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_SCHEMA = os.getenv("POSTGRES_SCHEMA")
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://" \
                           f"{POSTGRES_USER}:{POSTGRES_PASSWORD}@" \
                           f"{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'options': '-csearch_path={}'.format(POSTGRES_SCHEMA)})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
