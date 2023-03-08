b229ed50df092e6aef9663148eebeb783def6720
Milena Mansurova <mansurova@ebi.ac.uk>
2023-03-08 04:47:50 +0300

    fixes of pydantic schemas

--- kooplex-proxy/proxy/main.py
+++ kooplex-proxy/proxy/main.py
@@ -26,43 +26,43 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/country_samples/", response_model=List[schemas.MViewCountrySamples])
+@app.get("/country_samples/", response_model=List[schemas.MView_CountrySamples])
 def read_country_samples(skip: int = 0, limit: int = 100):
     country_samples = crud.get_country_samples(next(get_db()), skip=skip, limit=limit)
     return country_samples
 
 
-@app.get("/human_meta_mv/", response_model=List[schemas.MViewHumanMetaMv])
+@app.get("/human_meta_mv/", response_model=List[schemas.MView_HumanMetaMv])
 def read_meta(skip: int = 0, limit: int = 100):
     meta = crud.get_human_meta_mv(next(get_db()), skip=skip, limit=limit)
     return meta
 
 
-@app.get("/human_meta_mv_jhd/", response_model=List[schemas.MViewHumanMetaMvJhd])
+@app.get("/human_meta_mv_jhd/", response_model=List[schemas.MView_HumanMetaMvJhd])
 def read_meta(skip: int = 0, limit: int = 100):
     meta = crud.get_human_meta_mv_jhd(next(get_db()), skip=skip, limit=limit)
     return meta
 
 
-@app.get("/lineage_def/", response_model=List[schemas.LineageDef])
+@app.get("/lineage_def/", response_model=List[schemas.Table_LineageDef])
 def read_lineage_def(skip: int = 0, limit: int = 100):
     lineage_def = crud.get_lineage_def(next(get_db()), skip=skip, limit=limit)
     return lineage_def
 
 
-@app.get("/lineage/", response_model=List[schemas.MViewLineage])
+@app.get("/lineage/", response_model=List[schemas.MView_Lineage])
 def read_lineage(skip: int = 0, limit: int = 100):
     lineage = crud.get_lineage(next(get_db()), skip=skip, limit=limit)
     return lineage
 
 
-@app.get("/new_cases_jhd/", response_model=List[schemas.MViewNewCasesJhd])
+@app.get("/new_cases_jhd/", response_model=List[schemas.MView_NewCasesJhd])
 def read_new_cases(skip: int = 0, limit: int = 100):
     new_cases = crud.get_new_cases_jhd(next(get_db()), skip=skip, limit=limit)
     return new_cases
 
 
-@app.get("/variants_weekly/", response_model=List[schemas.MViewVariantsWeekly])
+@app.get("/variants_weekly/", response_model=List[schemas.MView_VariantsWeekly])
 def read_variants_weekly(skip: int = 0, limit: int = 100):
     variants_weekly = crud.get_variants_weekly(next(get_db()), skip=skip, limit=limit)
     return variants_weekly
--- kooplex-proxy/proxy/schemas.py
+++ kooplex-proxy/proxy/schemas.py
@@ -4,42 +4,42 @@ from pydantic import BaseModel
 from typing import Optional
 
 
-class MViewCountrySamples(BaseModel):
-    country = Optional[str]
-    n_sample = Optional[int]
-    log_n_sample = Optional[float]
-    relative_n_sample = Optional[float]
-    relative_log_n_sample = Optional[float]
+class MView_CountrySamples(BaseModel):
+    country: Optional[str]
+    n_sample: Optional[int]
+    log_n_sample: Optional[float]
+    relative_n_sample: Optional[float]
+    relative_log_n_sample: Optional[float]
 
     class Config:
         orm_mode = True
 
 
-class MViewHumanMetaMv(BaseModel):
-    country_name = Optional[str]
-    date = Optional[date]
-    date_year = Optional[float]
-    date_week = Optional[float]
-    weekly_sample = Optional[int]
+class MView_HumanMetaMv(BaseModel):
+    country_name: Optional[str]
+    date: Optional[date]
+    date_year: Optional[float]
+    date_week: Optional[float]
+    weekly_sample: Optional[int]
 
     class Config:
         orm_mode = True
 
 
-class MViewHumanMetaMvJhd(BaseModel):
-    country_name = Optional[str]
-    date = Optional[date]
-    date_year = Optional[float]
-    date_week = Optional[float]
-    weekly_sample = Optional[int]
-    cases = Optional[int]
-    pct = Optional[int]
+class MView_HumanMetaMvJhd(BaseModel):
+    country_name: Optional[str]
+    date: Optional[date]
+    date_year: Optional[float]
+    date_week: Optional[float]
+    weekly_sample: Optional[int]
+    cases: Optional[int]
+    pct: Optional[int]
 
     class Config:
         orm_mode = True
 
 
-class LineageDef(BaseModel):
+class Table_LineageDef(BaseModel):
     variant_id: Optional[str]
     pango: Optional[str]
     type_variant: Optional[str]
@@ -60,7 +60,7 @@ class LineageDef(BaseModel):
         orm_mode = True
 
 
-class MViewLineage(BaseModel):
+class MView_Lineage(BaseModel):
     clean_collection_date: Optional[date]
     clean_country: Optional[str]
     variant_id: Optional[str]
@@ -72,19 +72,19 @@ class MViewLineage(BaseModel):
         orm_mode = True
 
 
-class MViewNewCasesJhd(BaseModel):
+class MView_NewCasesJhd(BaseModel):
     country_name: Optional[str]
-    date = Optional[date]
-    date_year = Optional[float]
-    date_week = Optional[float]
-    weekly_sample = Optional[int]
-    cases = Optional[int]
+    date: Optional[date]
+    date_year: Optional[float]
+    date_week: Optional[float]
+    weekly_sample: Optional[int]
+    cases: Optional[int]
 
     class Config:
         orm_mode = True
 
 
-class MViewVariantsWeekly(BaseModel):
+class MView_VariantsWeekly(BaseModel):
     country_name: Optional[str]
     date_year: Optional[float]
     date_week: Optional[float]
