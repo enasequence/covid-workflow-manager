74c14471144c5065f3ba95b6068722c699834a84
Milena Mansurova <mansurova@ebi.ac.uk>
2023-04-05 04:52:40 +0300

    schema_changing function to avoid code repetition

--- kooplex-proxy/proxy/crud.py
+++ kooplex-proxy/proxy/crud.py
@@ -1,17 +1,21 @@
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.declarative import DeclarativeMeta
 
 import models
 from database import SCHEMA_SET
 
 
-def get_country_samples(db: Session, skip: int = 0, limit: int = 100, endp_schema='sandbox_public'):
-
-    current_schema = models.MViewCountrySamples.get_schema()
+def schema_changing(model: DeclarativeMeta, endp_schema: str):
+    current_schema = model.get_schema()
     if current_schema != endp_schema and endp_schema in SCHEMA_SET:
-        models.MViewCountrySamples.set_schema(schema_name=endp_schema)
+        model.set_schema(schema_name=endp_schema)
         print(f"-- Changing schema from '{current_schema}' to '{endp_schema}' for the endpoint: country_samples --")
 
-    return db.query(models.MViewCountrySamples).offset(skip).limit(limit).all()
+
+def get_country_samples(db: Session, skip: int = 0, limit: int = 100, endp_schema='sandbox_public'):
+    model = models.MViewCountrySamples
+    schema_changing(model=model, endp_schema=endp_schema)
+    return db.query(model).offset(skip).limit(limit).all()
 
 
 def get_human_meta_mv(db: Session, skip: int = 0, limit: int = 100):
